91e27339657866fc54290adf1065ee6005774c69
Ashley Wu <ashleywu2004@gmail.com>
2024-10-13 16:55:51 -0700

    refactor code

--- game.js
+++ game.js
@@ -1,5 +1,6 @@
 const buttonColours = ["red", "blue", "green", "yellow"];
 const gamePattern = [];
+const userPattern = [];
 
 function nextSequence() {
     const randomNumber = Math.floor(Math.random() * 4);
@@ -7,13 +8,24 @@ function nextSequence() {
     gamePattern.push(randomChosenColour);
 
     $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
-    var audio = new Audio("./sounds/" + randomChosenColour + ".mp3");
-    audio.play();
+    playSound(randomChosenColour);
 }
 
 $(".btn").on("click", function() {
     userPattern.push(this.id);
     animatePress(this.id);
-    var audio = new Audio("./sounds/" + this.id + ".mp3");
-    audio.play();
+    playSound(this.id);
 });
+
+function playSound(colour) {
+    var audio = new Audio("./sounds/" + colour + ".mp3");
+    audio.play();
+}
+
+function animatePress(currentColour) {
+    const currentButton = $("#" + currentColour);
+    currentButton.addClass("pressed");
+    setTimeout(() => {
+        currentButton.removeClass("pressed");
+    }, 100)
+}
