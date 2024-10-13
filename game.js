0ebf1bd8c09b3d0f5de6431883fc3e0292a9b502
Ashley Wu <ashleywu2004@gmail.com>
2024-10-13 16:52:58 -0700

    add animation to buttons

--- game.js
+++ game.js
@@ -5,4 +5,15 @@ function nextSequence() {
     const randomNumber = Math.floor(Math.random() * 4);
     const randomChosenColour = buttonColours[randomNumber];
     gamePattern.push(randomChosenColour);
+
+    $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
+    var audio = new Audio("./sounds/" + randomChosenColour + ".mp3");
+    audio.play();
 }
+
+$(".btn").on("click", function() {
+    userPattern.push(this.id);
+    animatePress(this.id);
+    var audio = new Audio("./sounds/" + this.id + ".mp3");
+    audio.play();
+});
