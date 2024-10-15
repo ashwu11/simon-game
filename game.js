9a4871a4f68bab369b2670a3687ecaa581965ce8
Ashley Wu <ashleywu2004@gmail.com>
2024-10-14 17:48:31 -0700

    implement start game

--- game.js
+++ game.js
@@ -1,8 +1,21 @@
 const buttonColours = ["red", "blue", "green", "yellow"];
 const gamePattern = [];
 const userPattern = [];
+let started = false;
+let level = 0;
+
+$(document).keypress(function() {
+    if (!started) {
+        $("#level-title").text("Level 0");
+        nextSequence();
+        started = true;
+    }
+})
 
 function nextSequence() {
+    level++;
+    $("#level-title").text("Level " + level);
+
     const randomNumber = Math.floor(Math.random() * 4);
     const randomChosenColour = buttonColours[randomNumber];
     gamePattern.push(randomChosenColour);
@@ -11,7 +24,7 @@ function nextSequence() {
     playSound(randomChosenColour);
 }
 
-$(".btn").on("click", function() {
+$(".btn").click(function() {
     userPattern.push(this.id);
     animatePress(this.id);
     playSound(this.id);
